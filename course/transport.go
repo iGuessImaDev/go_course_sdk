a24f79576a18c9eff9ec6a7946d4521c557f62c0
Sebastián Gallardo <iguesimadev@gmail.com>
2026-09-04 12:47:40 -0400

    fix error handling

--- course/transport.go
+++ course/transport.go
@@ -50,16 +50,16 @@ func (c *ClientHTTP) Get(id string) (*domain.Course, error) {
 		return nil, resp.Err
 	}
 
-	if resp.StatusCode == 404 {
-		return nil, ErrNotFound{fmt.Sprintf("%s", resp)}
-	}
-
-	if resp.StatusCode > 299 {
+	if err := resp.FillUp(&dataResponse); err != nil {
 		return nil, fmt.Errorf("%s", resp)
 	}
 
-	if err := resp.FillUp(&dataResponse); err != nil {
-		return nil, err
+	if resp.StatusCode == 404 {
+		return nil, ErrNotFound{fmt.Sprintf("%s", dataResponse.Message)}
+	}
+
+	if resp.StatusCode > 299 {
+		return nil, fmt.Errorf("%s", dataResponse.Message)
 	}
 
 	return dataResponse.Data.(*domain.Course), nil
